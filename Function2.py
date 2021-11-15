1048a2b8de396f95a00d2a190e43990cfde7282e
sullha <9.sull.9@gmail.com>
2021-11-15 16:51:02 +0900

    월요일 수업 완료

--- Function2.py
+++ Function2.py
@@ -12,3 +12,17 @@ def times(a,b):
     return a+b, a*b
 
 #호출
+result=times(3,4)
+print(result)
+
+#참조를 전달(Pass by Reference)
+def change(x):
+    #내부에 복사본 생성
+    x1=x[:]
+    x1[0] = "H"
+    print("함수 내부:", x1)
+
+#함수를 호출
+wordlist = ["J","A","M"]
+change(wordlist)
+print("함수 호출후:", wordlist)
