982057d9cda61fa7120ecb57b79b775d0842527d
sullha <9.sull.9@gmail.com>
2021-11-18 13:47:52 +0900

    오늘의 유머 크롤링

--- 오늘의유머검색.py
+++ 오늘의유머검색.py
@@ -6,9 +6,9 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
+for n in range(1,11):
         #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -18,21 +18,20 @@ for n in range(0,10):
         page = data.decode('utf-8', 'ignore')#utf-8로 디코드할거고 조금 깨지는건 무시해
         soup = BeautifulSoup(page, 'html.parser')
         #attrs는 속성들을 가리킴 (ex>class)
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        list = soup.find_all('td', attrs={'class':'subject'})
 
 
-
-        # <span class="subject_fixed" data-role="list-title-text" title="자급제 LG윙 폰 팝니다.(후면 + 전면 액정 교체 완료)">
-	# 		자급제 LG윙 폰 팝니다.(후면 + 전면 액정 교체 완료)
-	# </span>
+        # <td class="subject">
+        # <a href="/board/view.php">오징어게임 감독도 이명박근혜의 블랙리스트</a>
+        # </td>
 
 
         for item in list:
                 try:
-                        title = item.text.strip()
-                        if (re.search('아이폰', title)):
-                                 print(title.strip())
-                        #         print('https://www.clien.net'  + item['href'])
+                        title = item.find("a").text.strip()
+                        #print(title.strip())
+                        if (re.search('오징어', title)):
+                            print(title.strip())
                 except:
                         pass
         
