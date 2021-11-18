a613e2edf84ebe65957326c8750ee4fe842b987d
sullha <9.sull.9@gmail.com>
2021-11-18 15:18:10 +0900

    QT연습2

---
+++ DemoForm.py
@@ -0,0 +1,22 @@
+# DemoForm.py
+# DemoForm.ui(화면단) + DemoForm.py(로직단)
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+#화면을 로딩
+form_class = uic.loadUiType("DemoForm.ui")[0]
+
+#폼클래스(윈도우)를 정의
+class DemoForm(QDialog, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.label.setText("첫번째 데모")
+
+#진입점을 체크
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demoWindow = DemoForm()
+    demoWindow.show()
+    app.exec_()
